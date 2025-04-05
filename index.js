6f3470c55ef2826e3520949d455834e423b1e0d8
erickmagalhaes81 <erickmagalhaes81@gmail.com>
2025-04-04 23:12:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Sistema da Malharia Bloren</h1>
+      <p>Projeto online com Next.js!</p>
+    </div>
+  );
+}
