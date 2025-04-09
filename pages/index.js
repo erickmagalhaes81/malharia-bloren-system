60cd5580ff0df7fe501e3e474a9493c938022090
erickmagalhaes81 <erickmagalhaes81@gmail.com>
2025-04-09 18:44:23 -0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Sistema da Malharia Bloren</h1>
-      <p>Projeto online com Next.js!</p>
-    </div>
-  );
-}
