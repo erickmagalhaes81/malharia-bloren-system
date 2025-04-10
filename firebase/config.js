3028fc153af1bea0de879bb25c6d4e200cec0851
erickmagalhaes81 <erickmagalhaes81@gmail.com>
2025-04-10 15:10:49 -0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxXdjWdz9qKYatR6obNI451KJgKBRorj0",
+  apiKey: "AIzaSyDxXdyWd2gKYaR6obhI45IKJgBRorj0",
   authDomain: "sistema-malharia-bloren.firebaseapp.com",
   projectId: "sistema-malharia-bloren",
   storageBucket: "sistema-malharia-bloren.appspot.com",
   messagingSenderId: "631847397577",
   appId: "1:631847397577:web:9ee8c5b5249a43ee72aa8",
-  measurementId: "G-WNQYKK4LZ2" // opcional, pode remover se não for usar Analytics
+  measurementId: "G-WNQYKK4LZ2"
 };
 
 const app = initializeApp(firebaseConfig);
