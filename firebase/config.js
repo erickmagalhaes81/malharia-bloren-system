b518a4f71f08c4871c4a6fcec3cc05800cb7333e
erickmagalhaes81 <erickmagalhaes81@gmail.com>
2025-04-10 14:43:45 -0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,14 +1,9 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD0xjydg2YktR6obnI4S1KJgBRorj0",
+  apiKey: "AIzaSyDxXdyWdZgQykYaR6obhI45IKJgBRorj0",
   authDomain: "sistema-malharia-bloren.firebaseapp.com",
   projectId: "sistema-malharia-bloren",
   storageBucket: "sistema-malharia-bloren.appspot.com",
   messagingSenderId: "631847397577",
-  appId: "1:631847397577:web:9ee8c5b5249a43ee72aa8"
+  appId: "1:631847397577:web:9ee8c5b5249a43ee72aa8",
+  measurementId: "G-WNQYKXL4Z2"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
