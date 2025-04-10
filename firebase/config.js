3623214055b148a5e52ade2dd48bd82cbe67d4ef
erickmagalhaes81 <erickmagalhaes81@gmail.com>
2025-04-09 21:49:52 -0300

    Add files via upload

--- firebase/config.js
+++ firebase/config.js
@@ -1,13 +1,14 @@
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0XjyWdz9gKYatR6obN14SIKJgBRorj0",
+  apiKey: "AIzaSyD0xyjWd2g9KYatR6b0nN145K1jQBRorj0",
   authDomain: "sistema-malharia-bloren.firebaseapp.com",
   projectId: "sistema-malharia-bloren",
   storageBucket: "sistema-malharia-bloren.appspot.com",
-  messagingSenderId: "631847379577",
-  appId: "1:631847379577:web:9ee8c5b5249a43ee72aa8"
+  messagingSenderId: "631847397577",
+  appId: "1:631847397577:web:9ee8c5b5249a43ee72aa8"
 };
 
 const app = initializeApp(firebaseConfig);
