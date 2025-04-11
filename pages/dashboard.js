ac4af8b7391b64a473b87c5cefba48856d892ca0
erickmagalhaes81 <erickmagalhaes81@gmail.com>
2025-04-10 22:16:21 -0300

    Add files via upload

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,3 +1,13 @@
+import Sidebar from '../components/Sidebar';
+
 export default function Dashboard() {
-  return <h1>Painel Administrativo</h1>;
+  return (
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
+      <main style={{ padding: '20px', width: '100%' }}>
+        <h1>Painel Administrativo</h1>
+        <p>Bem-vindo ao sistema da Malharia Bloren.</p>
+      </main>
+    </div>
+  );
 }
